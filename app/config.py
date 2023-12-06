8a07a77b5fbb5c9d17999ee6f3116c9f17a01737
Vital31 <>
2023-12-06 18:48:26 +0300

    +config

--- app/config.py
+++ app/config.py
@@ -4,14 +4,14 @@ SECRET_KEY = 'secret-key'
 
 
 #phpmyadmin от ПОЛИТЕХА
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://std_1865_sokap:12345678@std-mysql.ist.mospolytech.ru/std_1865_sokap'
+# SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://std_1865_sokap:12345678@std-mysql.ist.mospolytech.ru/std_1865_sokap'
 
 
 #локалка постгрес
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@localhost:5432/sok'
 
 #локалка mysql
-# SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:12345678@localhost:3306/sokap'
+SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://admin:123456@localhost:3306/test'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
