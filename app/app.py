bac36651af22cb60a0c3d34236f1be997d562592
Neretin Vlad <neretinvlad04@gmail.com>
2023-12-16 12:00:31 +0300

    test

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, current_user
 from datetime import datetime, timedelta
-
+#урааааааааааа cooommiiit
 
 app = Flask(__name__)
 application = app
