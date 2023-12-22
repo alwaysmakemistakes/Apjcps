dce8599b49d719155e9feccd7756dc33b0af27e3
Vital31 <>
2023-12-23 01:54:04 +0300

    addsql

---
+++ database/Apjcps1.sql
@@ -0,0 +1,331 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: std-mysql
+-- Время создания: Дек 22 2023 г., 22:31
+-- Версия сервера: 5.7.26-0ubuntu0.16.04.1
+-- Версия PHP: 8.1.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `std_1865_folk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `alembic_version`
+--
+
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `alembic_version`
+--
+
+INSERT INTO `alembic_version` (`version_num`) VALUES
+('8878d9d57d88');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `genres`
+--
+
+INSERT INTO `genres` (`id`, `name`) VALUES
+(7, 'Аниме опеннинги'),
+(5, 'Джаз'),
+(2, 'ПОП'),
+(3, 'РЕП'),
+(1, 'РОК'),
+(8, 'Фонк >:)'),
+(4, 'ХИП-ХОП'),
+(6, 'Частушки');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `images`
+--
+
+CREATE TABLE `images` (
+  `id` varchar(100) NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  `mime_type` varchar(100) NOT NULL,
+  `md5_hash` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `plastinkas`
+--
+
+CREATE TABLE `plastinkas` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `year` int(11) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `volume` int(11) NOT NULL,
+  `rating_sum` int(11) NOT NULL,
+  `rating_num` int(11) NOT NULL,
+  `id_image` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `plastinka_genre`
+--
+
+CREATE TABLE `plastinka_genre` (
+  `plastinka_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `plastinka_visits`
+--
+
+CREATE TABLE `plastinka_visits` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `plastinka_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `plastinka_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` text NOT NULL,
+  `role_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`, `role_description`) VALUES
+(1, 'Администратор', 'Суперпользователь, имеет полный доступ к системе, в том числе к созданию и удалению книг.'),
+(2, 'Исполнитель', 'Может редактировать данные пластинок'),
+(3, 'Пользователь', 'Может оставлять рецензии.');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `login` varchar(100) NOT NULL,
+  `password_hash` varchar(200) NOT NULL,
+  `role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `last_name`, `first_name`, `middle_name`, `login`, `password_hash`, `role_id`) VALUES
+(1, 'Иван1', 'Иван', 'Иван', 'user1', 'pbkdf2:sha256:260000$FNhdNYye9JvM4XuF$fadb80937ab27c142d8fb7b400c20a30ab753cd9609d9a521ec4d99e384c3b9e', 1),
+(2, 'Иван2', 'Иван', 'Иван', 'user2', 'pbkdf2:sha256:260000$VYf2IvDZCsqqKWha$ecb45a0c8366ec674b5b96af00caf5ed808bafe8bd0095ac5756599cd44e2881', 2),
+(3, 'Иван3', 'Иван', 'Иван', 'user3', 'pbkdf2:sha256:260000$SYHMkKjgGPElr2c0$844f1798858f08a37315bb649e05615776d6f39892879b2aab42e914db1ce8ed', 3);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `alembic_version`
+--
+ALTER TABLE `alembic_version`
+  ADD PRIMARY KEY (`version_num`);
+
+--
+-- Индексы таблицы `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Индексы таблицы `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `md5_hash` (`md5_hash`);
+
+--
+-- Индексы таблицы `plastinkas`
+--
+ALTER TABLE `plastinkas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_image` (`id_image`);
+
+--
+-- Индексы таблицы `plastinka_genre`
+--
+ALTER TABLE `plastinka_genre`
+  ADD PRIMARY KEY (`plastinka_id`,`genre_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Индексы таблицы `plastinka_visits`
+--
+ALTER TABLE `plastinka_visits`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `plastinka_id` (`plastinka_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `plastinka_id` (`plastinka_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `plastinkas`
+--
+ALTER TABLE `plastinkas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `plastinka_visits`
+--
+ALTER TABLE `plastinka_visits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `plastinkas`
+--
+ALTER TABLE `plastinkas`
+  ADD CONSTRAINT `plastinkas_ibfk_1` FOREIGN KEY (`id_image`) REFERENCES `images` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `plastinka_genre`
+--
+ALTER TABLE `plastinka_genre`
+  ADD CONSTRAINT `plastinka_genre_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `plastinka_genre_ibfk_2` FOREIGN KEY (`plastinka_id`) REFERENCES `plastinkas` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `plastinka_visits`
+--
+ALTER TABLE `plastinka_visits`
+  ADD CONSTRAINT `plastinka_visits_ibfk_1` FOREIGN KEY (`plastinka_id`) REFERENCES `plastinkas` (`id`),
+  ADD CONSTRAINT `plastinka_visits_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`plastinka_id`) REFERENCES `plastinkas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
